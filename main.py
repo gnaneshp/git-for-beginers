199cf7ae76506ccd40d73a1145e842cb1fba36af
gnaneshp <gnaneshp789@gmail.com>
2026-09-01 13:09:04 +0530

    added descriptions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils.calculator import add_nums,divide_nums,subtract_nums,power,multiply_nums
 
 if __name__ =="__main__":
-    print(add_nums(1,3))
-    print(divide_nums(1,3))
-    print(subtract_nums(1,3))
-    print(power(1,3))
-    print(multiply_nums(1,3))
+    print("addition:",add_nums(1,3))
+    print("division:",divide_nums(1,3))
+    print("subtraction:",subtract_nums(1,3))
+    print("power:",power(1,3))
+    print("multiplication:",multiply_nums(1,3))
